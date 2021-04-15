e12413d6b3039c7c4d3a2f2501f1dbd38e154099
Arthur Batista <arthurbat@gmail.com>
2021-04-15 14:03:40 -0400

    Handling name attribute

--- terraform/data_transformation.py
+++ terraform/data_transformation.py
@@ -7,10 +7,15 @@ def handler(event, context):
     attributes = ['id', 'name', 'abv', 'ibu', 'target_fg',
                   'target_og', 'ebc', 'srm', 'ph']
     
+    def quote_str(value):
+        if isinstance(value, str):
+            value = f'"{value}"'
+        return str(value)
+
     for record in event['records']:
         payload = json.loads(base64.b64decode(record['data']).decode("utf-8"))
         payload = payload[0]
-        csv_payload = ','.join([str(payload.get(key)) for key in attributes]) + '\n'
+        csv_payload = ','.join([quote_str(payload.get(key)) for key in attributes]) + '\n'
         csv_payload = base64.b64encode(csv_payload.encode('utf-8')).decode('utf-8')
         
         output_record = {
